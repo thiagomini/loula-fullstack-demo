c2d5025f2655732955d5cc4584b4877d1b040e71
Thiago Martins <thiago_valentim1@hotmail.com>
2024-05-09 17:59:07 -0300

    chore(backend): add wage access request table

---
+++ apps/backend/migrations/20240509175524-create-wage-access-request-table.ts
@@ -0,0 +1,22 @@
+import { Kysely } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable('wage_access_request')
+    .addColumn('id', 'uuid', (col) => col.primaryKey())
+    .addColumn('employee_id', 'uuid', (col) => col.notNull())
+    .addColumn('requested_amount', 'float8', (col) => col.notNull())
+    .addColumn('requested_currency', 'varchar', (col) => col.notNull())
+    .addForeignKeyConstraint(
+      'employee_id_fk',
+      ['employee_id'],
+      'employee',
+      ['id'],
+      (cb) => cb.onDelete('cascade'),
+    )
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable('wage_access_request').execute();
+}
